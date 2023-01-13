cfd7c570511a25f0138149eba8766bd75d8db064
jeenie <devjeenie@gmail.com>
2023-01-14 04:31:23 +0900

    update NavBar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,23 +1,35 @@
+import { ToggleButton, ToggleButtonGroup } from "@mui/material";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function NavBar() {
   const router = useRouter();
   return (
-    <nav>
-      <img src="/vercel.svg" />
+    <ToggleButtonGroup size="large">
       <Link
         href="/"
-        // style={{ color: router.pathname === "/" ? "red" : "blue" }}
+        style={{ color: router.pathname === "/" ? "red" : "blue" }}
       >
-        Home
+        <ToggleButton value="home">Home</ToggleButton>
       </Link>
       <Link
         href="/about"
-        // style={{ color: router.pathname === "/about" ? "red" : "blue" }}
+        style={{ color: router.pathname === "/about" ? "red" : "blue" }}
       >
-        About
+        <ToggleButton value="about">About</ToggleButton>
       </Link>
-    </nav>
+      <Link
+        href="/signin"
+        style={{ color: router.pathname === "/signin" ? "red" : "blue" }}
+      >
+        <ToggleButton value="signin">Sign in</ToggleButton>
+      </Link>
+      <Link
+        href="/signup"
+        style={{ color: router.pathname === "/signup" ? "red" : "blue" }}
+      >
+        <ToggleButton value="signup">Sign up</ToggleButton>
+      </Link>
+    </ToggleButtonGroup>
   );
 }
