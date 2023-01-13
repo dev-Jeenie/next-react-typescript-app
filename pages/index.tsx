777a854ce44c93390056b36424532ec795b13b70
jeenie <devjeenie@gmail.com>
2023-01-14 04:04:24 +0900

    add getServerSideProps for SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,26 +2,27 @@ import { Button } from "@mui/material";
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-export default function Home() {
-  const [movies, setMovies] = useState<any[]>([]);
-
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-    })();
-  }, []);
-
+export default function Home({ results }: { results: any }) {
   return (
     <div>
       <Seo title="Home" />
       <Button variant="contained">Contained</Button>
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+      {results?.map((movie: any) => (
         <div key={movie.id}>
           <h4>{movie.original_title}</h4>
         </div>
       ))}
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
